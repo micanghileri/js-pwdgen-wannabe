bd229dbb732f89052c48adf4bb0b15e4e0a90969
Michele Anghileri <micheleanghileri@gmail.com>
2020-07-06 16:14:34 +0200

    definitivo

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ var favouriteColour = prompt("Qual è il tuo colore preferito?");
 
 var sum = name + surname + favouriteColour + 19;
 
-document.getElementById('pswresult').innerHTML = 'la tua password insicura è:'+" "+ sum;
+document.getElementById('pswresult').innerHTML = 'La tua password insicura è:'+" "+ sum;
 
-console.log (name);
-console.log (surname);
-console.log (favouriteColour);
+// console.log (name);
+// console.log (surname);
+// console.log (favouriteColour);
